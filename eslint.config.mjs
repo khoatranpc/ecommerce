59fec18eef4906c141d240aacf1dfbce2400c669
khoatranpc <khoatranpc603@gmail.com>
2025-03-08 21:38:40 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,18 +14,19 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/consistent-type-imports': 'warn',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react-hooks/exhaustive-deps': "off",
-      '@typescript-eslint/consistent-type-imports': 'off',
-      'react-hooks/rules-of-hooks': 'off',
-      "@typescript-eslint/no-unsafe-function-type": 'off'
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/consistent-type-imports": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
     plugins: {
-      '@typescript-eslint': eslintPluginTs,
-    }
+      "@typescript-eslint": eslintPluginTs,
+    },
   },
 ];
 
